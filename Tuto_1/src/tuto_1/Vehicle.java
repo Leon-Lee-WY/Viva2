87fb86325846a889c1cad0a3d07cc2d941e2b9cf
lwylw <lwylw@Yong120506>
2026-03-17 08:13:32 +0800

    finished

---
+++ Tuto_1/src/tuto_1/Vehicle.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tuto_1;
+
+/**
+ *
+ * @author lwylw
+ */
+public class Vehicle {
+    
+}
